47640d900091dcb08d9613a12551216f20924569
Martin Czygan <martin.czygan@gmail.com>
2016-11-05 12:38:03 +0100

    simple perceptron hypothesis

---
+++ code/perceptron.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+A simple perceptron.
+"""
+
+def dot(a, b):
+    """ Dot product. """
+    if not len(a) == len(b):
+        raise ValueError('vectors must be of same length')
+    return sum([a * b for a, b in zip(a, b)])
+
+def sign(x):
+    if x >= 0:
+        return 1
+    return -1
+
+def h(x, w, b):
+    """ Hypothesis. """
+    return sign(dot(x, w) + b)
+
+if __name__ == '__main__':
+    # measurements
+    X = [
+        [0.5, 1.0, 0.2],
+        [0.7, 1.0, 0.3],
+        [0.2, 0.5, 0.3],
+        [0.4, 0.6, 0.2],
+    ]
+
+    # measurement class
+    y = [-1, -1, 1, 1]
+
+    # weight of the model
+    W = [0.1, 0.1, 0.1]
+    b = 0.1
+    
+    for i, x in enumerate(X):
+        yhat = h(x, W, b)
+        print(yhat, y[i])
