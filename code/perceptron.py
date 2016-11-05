fcf07ba569252eee73a327dbe8f158170a4eee0d
Martin Czygan <martin.czygan@gmail.com>
2016-11-05 13:00:10 +0100

    first, buggy PLA

--- code/perceptron.py
+++ code/perceptron.py
@@ -8,17 +8,31 @@ A simple perceptron.
 def dot(a, b):
     """ Dot product. """
     if not len(a) == len(b):
-        raise ValueError('vectors must be of same length')
+        raise ValueError('vectors must be of same length, got %d and %d' % (len(a), len(b)))
     return sum([a * b for a, b in zip(a, b)])
 
 def sign(x):
     if x >= 0:
         return 1
-    return -1
+    return 0
 
 def h(x, w, b):
     """ Hypothesis. """
     return sign(dot(x, w) + b)
+
+def pla(X, y):
+    """
+    Given a dataset, return model.
+    """
+    # initialize model
+    weights, bias = [0 for _ in X[0]], 0
+
+    for i, x in enumerate(X):
+        yhat = h(x, weights, bias)
+        for j, w in enumerate(weights):
+            weights[j] = weights[j] + (y[i] - yhat) * x[j]
+
+    return weights, bias
 
 if __name__ == '__main__':
     # measurements
@@ -30,11 +44,13 @@ if __name__ == '__main__':
     ]
 
     # measurement class
-    y = [-1, -1, 1, 1]
+    y = [0, 0, 1, 1]
 
     # weight of the model
     W = [0.1, 0.1, 0.1]
     b = 0.1
+
+    W, b = pla(X, y)
     
     for i, x in enumerate(X):
         yhat = h(x, W, b)
