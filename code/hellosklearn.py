9e951ee5df0bf0af41bc753c1a459dc86ebcd3ac
Martin Czygan <martin.czygan@gmail.com>
2016-11-07 17:58:45 +0100

    sklearn, stub

--- code/hellosklearn.py
+++ code/hellosklearn.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from sklearn.neural_network import MLPClassifier
+from sklearn.datasets import fetch_mldata
+
+if __name__ == '__main__':
+    mnist = fetch_mldata('MNIST original')
+    split = 60000
+
+    X, y = MNIST.data / MNIST.data.max(), MNIST.target
+
+    X_train, X_test = X[:split], X[split:]
+    Y_train, Y_test = Y[:split], y[split:]
+
+    mlp = MLPClassifier(hidden_layer_sizes=(n_units, n_layers),
+                        max_iter=n_iterations,
+                        alpha=1e-4,
+                        solver=solver,
+                        ...)
