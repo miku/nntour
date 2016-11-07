5eb2a5578e5855e1df4346c3770ff078a0b3812b
Martin Czygan <martin.czygan@gmail.com>
2016-11-08 00:20:33 +0100

    some more on sklearn

--- code/hellosklearn.py
+++ code/hellosklearn.py
@@ -5,16 +5,97 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import fetch_mldata
 
 if __name__ == '__main__':
+    # stores data in ~/scikit_learn_data by default
     mnist = fetch_mldata('MNIST original')
     split = 60000
 
-    X, y = MNIST.data / MNIST.data.max(), MNIST.target
+    X, y = mnist.data / 255., mnist.target
 
     X_train, X_test = X[:split], X[split:]
-    Y_train, Y_test = Y[:split], y[split:]
+    y_train, y_test = y[:split], y[split:]
 
-    mlp = MLPClassifier(hidden_layer_sizes=(n_units, n_layers),
-                        max_iter=n_iterations,
-                        alpha=1e-4,
-                        solver=solver,
-                        ...)
+    # mlp = MLPClassifier(verbose=10)
+
+    mlp = MLPClassifier(verbose=10,
+                        hidden_layer_sizes=(100, 100),
+                        max_iter=400, alpha=1e-4,
+                        solver='sgd',
+                        tol=1e-4,
+                        random_state=1,
+                        learning_rate_init=.1)
+
+    mlp.fit(X_train, y_train)
+
+    print("training set score: %0.6f" % mlp.score(X_train, y_train))
+    print("test set score: %0.6f" % mlp.score(X_test, y_test))
+
+# MLPClassifier with defaults
+# ---------------------------
+#
+# Iteration 1, loss = 0.42878618
+# Iteration 2, loss = 0.20505457
+# Iteration 3, loss = 0.15200607
+# Iteration 4, loss = 0.12161441
+# Iteration 5, loss = 0.10157177
+# Iteration 6, loss = 0.08580176
+# Iteration 7, loss = 0.07357175
+# Iteration 8, loss = 0.06570893
+# Iteration 9, loss = 0.05695424
+# Iteration 10, loss = 0.05000736
+# Iteration 11, loss = 0.04464689
+# Iteration 12, loss = 0.04013500
+# Iteration 13, loss = 0.03532952
+# Iteration 14, loss = 0.03185975
+# Iteration 15, loss = 0.02797278
+# Iteration 16, loss = 0.02470729
+# Iteration 17, loss = 0.02242617
+# Iteration 18, loss = 0.02026662
+# Iteration 19, loss = 0.01808858
+# Iteration 20, loss = 0.01541741
+# Iteration 21, loss = 0.01444279
+# Iteration 22, loss = 0.01286522
+# Iteration 23, loss = 0.01087826
+# Iteration 24, loss = 0.01033050
+# Iteration 25, loss = 0.00887871
+# Iteration 26, loss = 0.00810440
+# Iteration 27, loss = 0.00696718
+# Iteration 28, loss = 0.00619607
+# Iteration 29, loss = 0.00622639
+# Iteration 30, loss = 0.00530486
+# Iteration 31, loss = 0.00490973
+# Iteration 32, loss = 0.00426621
+# Iteration 33, loss = 0.00383416
+# Iteration 34, loss = 0.00344564
+# Iteration 35, loss = 0.00342458
+# Iteration 36, loss = 0.00443835
+# Iteration 37, loss = 0.00375365
+# Training loss did not improve more than tol=0.000100 for two consecutive epochs. Stopping.
+# training set score: 0.999783
+# test set score: 0.978400
+
+# MLPClassifier, 2 hidden layers, 100 units each
+# ----------------------------------------------
+
+# Iteration 1, loss = 0.28833772
+# Iteration 2, loss = 0.10794344
+# Iteration 3, loss = 0.07889505
+# Iteration 4, loss = 0.06081647
+# Iteration 5, loss = 0.05005079
+# Iteration 6, loss = 0.03929399
+# Iteration 7, loss = 0.03312722
+# Iteration 8, loss = 0.02458840
+# Iteration 9, loss = 0.02280704
+# Iteration 10, loss = 0.01994174
+# Iteration 11, loss = 0.01521781
+# Iteration 12, loss = 0.01271448
+# Iteration 13, loss = 0.01315993
+# Iteration 14, loss = 0.00956860
+# Iteration 15, loss = 0.00867025
+# Iteration 16, loss = 0.00954227
+# Iteration 17, loss = 0.00595672
+# Iteration 18, loss = 0.00814455
+# Iteration 19, loss = 0.00635555
+# Iteration 20, loss = 0.00887355
+# Training loss did not improve more than tol=0.000100 for two consecutive epochs. Stopping.
+# training set score: 0.998233
+# test set score: 0.978500
---
+++ code/sknngrid.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Grid search with NN.
+"""
+
+from sklearn.datasets import fetch_mldata
+from sklearn.model_selection import GridSearchCV
+from sklearn.neural_network import MLPClassifier
+import json
+import numpy as np
+
+class SeqEncoder(json.JSONEncoder):
+    """
+    Helper to encode additional types.
+    
+    > To use a custom JSONDecoder subclass, specify it with the cls kwarg;
+    > otherwise JSONDecoder is used. https://is.gd/X1GZXX
+    """
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+        if isinstance(obj, np.array):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+if __name__ == '__main__':
+    # stores data in ~/scikit_learn_data by default
+    mnist = fetch_mldata('MNIST original')
+    split = 60000
+
+    X, y = mnist.data / 255., mnist.target
+
+    X_train, X_test = X[:split], X[split:]
+    y_train, y_test = y[:split], y[split:]
+
+    parameters = {
+        # 'hidden_layer_sizes': ((1,), (10,), (100,)),
+        'hidden_layer_sizes': ((1,)),
+        # 'activation': ('relu', 'tanh'),
+        'activation': ('relu',),
+    }
+
+    mlp = MLPClassifier()
+    clf = GridSearchCV(mlp, parameters, verbose=10, n_jobs=4)
+
+    clf.fit(X_train, y_train)
+
+    print(json.dumps(clf.cv_results_, cls=SeqEncoder))
+
+# Fitting 3 folds for each of 6 candidates, totalling 18 fits
+# [CV] activation=relu, hidden_layer_sizes=(1,) ........................
+# [CV] activation=relu, hidden_layer_sizes=(1,) ........................
+# [CV] activation=relu, hidden_layer_sizes=(1,) ........................
+# [CV] activation=relu, hidden_layer_sizes=(10,) .......................
+# [CV]  activation=relu, hidden_layer_sizes=(1,), score=0.371419 -   0.1s
+# [CV] activation=relu, hidden_layer_sizes=(10,) .......................
+# [CV]  activation=relu, hidden_layer_sizes=(10,), score=0.927065 -   0.1s
+# [CV] activation=relu, hidden_layer_sizes=(10,) .......................
+# [CV]  activation=relu, hidden_layer_sizes=(1,), score=0.382357 -   0.1s
+# [CV] activation=relu, hidden_layer_sizes=(100,) ......................
+# [CV]  activation=relu, hidden_layer_sizes=(1,), score=0.368126 -   0.1s
+# [CV] activation=relu, hidden_layer_sizes=(100,) ......................
+# [CV]  activation=relu, hidden_layer_sizes=(10,), score=0.927296 -   0.2s
+# [Parallel(n_jobs=4)]: Done   5 tasks      | elapsed:  2.4min
+# [CV] activation=relu, hidden_layer_sizes=(100,) ......................
+# [CV]  activation=relu, hidden_layer_sizes=(10,), score=0.930840 -   0.1s
+# [CV] activation=tanh, hidden_layer_sizes=(1,) ........................
+# [CV]  activation=relu, hidden_layer_sizes=(100,), score=0.972206 -   0.4s
+# [CV] activation=tanh, hidden_layer_sizes=(1,) ........................
+# [CV]  activation=relu, hidden_layer_sizes=(100,), score=0.969298 -   0.3s
+# [CV] activation=tanh, hidden_layer_sizes=(1,) ........................
+# [CV]  activation=relu, hidden_layer_sizes=(100,), score=0.972246 -   0.2s
+# [CV] activation=tanh, hidden_layer_sizes=(10,) .......................
+# [CV]  activation=tanh, hidden_layer_sizes=(1,), score=0.434263 -   0.1s
+# [Parallel(n_jobs=4)]: Done  10 tasks      | elapsed:  5.0min
+# [CV] activation=tanh, hidden_layer_sizes=(10,) .......................
+# [CV]  activation=tanh, hidden_layer_sizes=(1,), score=0.365218 -   0.0s
+# [CV] activation=tanh, hidden_layer_sizes=(10,) .......................
+# [CV]  activation=tanh, hidden_layer_sizes=(1,), score=0.415312 -   0.0s
+# [CV] activation=tanh, hidden_layer_sizes=(100,) ......................
+# [CV]  activation=tanh, hidden_layer_sizes=(10,), score=0.923665 -   0.1s
+# [Parallel(n_jobs=4)]: Done  13 out of  18 | elapsed:  6.3min remaining:  2.4min
+# [CV] activation=tanh, hidden_layer_sizes=(100,) ......................
+# [CV]  activation=tanh, hidden_layer_sizes=(10,), score=0.915696 -   0.1s
+# [CV] activation=tanh, hidden_layer_sizes=(100,) ......................
+# [CV]  activation=tanh, hidden_layer_sizes=(10,), score=0.921638 -   0.2s
+# [Parallel(n_jobs=4)]: Done  15 out of  18 | elapsed:  7.4min remaining:  1.5min
+# [CV]  activation=tanh, hidden_layer_sizes=(100,), score=0.972905 -   0.4s
+# [CV]  activation=tanh, hidden_layer_sizes=(100,), score=0.969548 -   0.3s
+# [CV]  activation=tanh, hidden_layer_sizes=(100,), score=0.969495 -   0.2s
+# [Parallel(n_jobs=4)]: Done  18 out of  18 | elapsed:  9.3min finished
